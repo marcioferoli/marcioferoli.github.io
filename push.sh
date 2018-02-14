f2c0289297a19dae2322ac79be364a58f9d59b6e
Márcio Oliveira <marcioferoli@gmail.com>
2018-02-14 17:57:21 -0200

    Automated commit on 2018-02-14 17:57:21 -02

--- push.sh
+++ push.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 # chmod 777 push.sh
+COMMIT_TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S %Z'`
+
+echo "Before"
 git status
+
+echo "Push on ${COMMIT_TIMESTAMP}"
 git add --all
-git commit -m "Auto Commit"
+git commit -m "Automated commit on ${COMMIT_TIMESTAMP}"
+git push -u origin master
+
+echo "After"
+git status
+
